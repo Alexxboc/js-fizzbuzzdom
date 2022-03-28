84feb2fec613676fbbdd35d8a50d1e8c700ec369
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-28 14:23:01 +0200

    Create FizzBuzz console log program

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 // Seleziona il container 
 const numbersElement = document.querySelector(`.container`)
 
-// Crea 100 quadrati con testo
+// Stampa un ciclo da 1 a 100
 for (let i = 1; i <= 100; i++){
-    console.log(i);
-    const markupLi = `
-    <div class="square">
-    <div>${i}</>
-    </div>`
-
-    numbersElement.insertAdjacentHTML
-    (`beforeend`, markupLi)
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log(`FizzBuzz`)
+    }
+    else if (i % 3 == 0) {
+        console.log(`Fizz`)
+    }
+    else if (i % 5 == 0) {
+        console.log(`Buzz`)
+    }
+    else {
+        console.log(i)
+    }
+    
 }
